f11dc4b9b470e64dbf459fd59e622d1cb1a56eae
orggue <jorgechato1@gmail.com>
2015-04-25 12:45:01 +0200

    chart

--- src/scripts/dashboard/my/chart.js
+++ src/scripts/dashboard/my/chart.js
@@ -1,5 +1,52 @@
-// 'use strict';
-//
-// angular.module('insulinum')
-// .controller('mainController', function ($scope) {
-// });
+'use strict';
+
+angular.module('insulinum')
+.controller('cardController', function ($scope) {
+    $scope.chartCard = [
+        {
+            'id':'line',
+            'class':'chart-line',
+            'name':'Linear chart',
+            'date' : new Date(),
+            'time' : new Date(),
+            'note' : 'something that happen in this control is everything I whant #EverythingOK'
+        },
+        {
+            'id':'bar',
+            'class':'chart-bar',
+            'name':'Bar chart',
+            'date' : new Date(),
+            'time' : new Date(),
+            'note' : 'something that happen in this control is everything I whant #EverythingOK'
+        },
+        {
+            'id':'radar',
+            'class':'chart-radar',
+            'name':'Radar chart',
+            'date' : new Date(),
+            'time' : new Date(),
+            'note' : 'something that happen in this control is everything I whant #EverythingOK'
+        },
+        {
+            'id':'pie',
+            'class':'chart-pie',
+            'name':'Pie chart',
+            'date' : new Date(),
+            'time' : new Date(),
+            'note' : 'something that happen in this control is everything I whant #EverythingOK'
+        },
+    ];
+
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+    $scope.series = ['Insulinum', 'Glucose'];
+    $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+    ];
+
+    $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+    };
+});
