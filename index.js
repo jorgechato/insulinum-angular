ea331f9861041776b33a092633e71e3807b7dfe6
orggue <jorgechato1@gmail.com>
2015-04-29 20:53:32 +0200

    automatic deploy

---
+++ index.js
@@ -0,0 +1,8 @@
+var gzippo = require('gzippo');
+var express = require('express');
+var logger = require('express-logger');
+var app = express();
+
+app.use(logger({path:"dev"}));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
