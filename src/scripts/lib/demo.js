861883d5a6140e8529b6e5a06d4db437b002e4f4
orggue <jorgechato1@gmail.com>
2015-05-10 09:48:53 +0200

    download each chart as png

--- src/scripts/lib/demo.js
+++ src/scripts/lib/demo.js
@@ -9,20 +9,20 @@ $(window).on('load',function(){
     NProgress.done();
 });
 
-function canvasToPdf(chart){
+function canvasToPdf(savebutton){
     // var pdf = new jsPDF('p','pt','a4');
     // pdf.addHTML($(chart),function() {
     //     pdf.output('datauri');
     // });
 
-    var canvas = $(chart);
+    var name = $(savebutton).val();
+    var canvas = $('#'+name);
 
-    chart = chart.replace('#','');
     var fullQuality = canvas[0].toDataURL("image/png", 1.0);
 
     var a = $("<a>")
     .attr("href", fullQuality)
-    .attr("download", chart+".png")
+    .attr("download", name + '-' + new Date() +".png")
     .appendTo("body");
 
     a[0].click();
